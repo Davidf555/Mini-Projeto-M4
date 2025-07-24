514933de2b4938b292042e4d0a16aefeb169df0e
Davidf555 <daviflavio555@gmail.com>
2025-07-24 00:13:43 -0300

    Rotas individuais

--- assets/route.js
+++ assets/route.js
@@ -51,4 +51,26 @@ router.get('/Tudo', (req, res) => {
     });
 });
 
+router.get('/acre', (req, res) => res.json(acre));
+router.get('/alagoas', (req, res) => res.json(alagoas));
+router.get('/amazonas', (req, res) => res.json(amazonas));
+router.get('/bahia', (req, res) => res.json(bahia));
+router.get('/ceara', (req, res) => res.json(ceara));
+router.get('/distrito-federal', (req, res) => res.json(distritoFederal));
+router.get('/espirito-santo', (req, res) => res.json(espiritoSanto));
+router.get('/goias', (req, res) => res.json(goias));
+router.get('/maranhao', (req, res) => res.json(maranhao));
+router.get('/mato-grosso-do-sul', (req, res) => res.json(matoGrossoDoSul));
+router.get('/minas-gerais', (req, res) => res.json(minasGerais));
+router.get('/paraiba', (req, res) => res.json(paraiba));
+router.get('/parana', (req, res) => res.json(parana));
+router.get('/pernambuco', (req, res) => res.json(pernambuco));
+router.get('/piaui', (req, res) => res.json(piaui));
+router.get('/rio-de-janeiro', (req, res) => res.json(rioDeJaneiro));
+router.get('/rio-grande-do-norte', (req, res) => res.json(rioGrandeDoNorte));
+router.get('/rio-grande-do-sul', (req, res) => res.json(rioGrandeDoSul));
+router.get('/santa-catarina', (req, res) => res.json(santaCatarina));
+router.get('/sao-paulo', (req, res) => res.json(saoPaulo));
+router.get('/sergipe', (req, res) => res.json(sergipe));
+
 module.exports = router;
